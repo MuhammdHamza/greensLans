1297c098793f4541a037120f22a434269daef234
Hamza amir <hamzaamir108@gmail.com>
2024-07-08 05:17:59 +0800

    initial changes

---
+++ cypress/e2e/Users/Add_users.cy.js
@@ -0,0 +1,41 @@
+/// <reference types="cypress" />
+// Import the Cypress XPath plugin
+
+require('cypress-xpath');
+
+describe('Test suite for login page', () => {
+    beforeEach(() => {
+        cy.visit('/'); // Replace with the actual URL or path
+        cy.login(); // Assuming cy.login() is defined in auth/login.cy.js
+    });
+    it("Test case # 1 : Verify that the validations are working fine or not", () => {
+        cy.get('[data-item-id="/users"] > .dx-item > .dx-item-content').click({ force: true });
+        cy.intercept("GET", "**/*").as('User');
+        cy.wait('@User');
+        cy.get('.buttons-container > .dx-button-has-text > .dx-button-content > .dx-icon').click({force:true});
+        cy.get('#submit-link > .dx-buttongroup > .dx-buttongroup-wrapper > .dx-button-has-text > .dx-button-content').click({force:true});
+    });
+    it.only("Test Case # 2 : Verify that the inputs are being inputted",()=>{
+        cy.get('[data-item-id="/users"] > .dx-item > .dx-item-content').click({ force: true });
+        cy.intercept("GET", "**/*").as('User');
+        cy.wait('@User');
+        cy.get('.buttons-container > .dx-button-has-text > .dx-button-content').click({force:true});
+        cy.get(':nth-child(1) > .popup-group-form-input').should('be.visible').click({force:true}); // email field
+        cy.get(':nth-child(1) > .popup-group-form-input').type('testinguser23@gmail.com');
+        cy.get(':nth-child(2) > .popup-group-form-input > .dx-show-invalid-badge > .dx-texteditor-container > .dx-texteditor-input-container > .dx-texteditor-input').click({force:true}); 
+        cy.get(':nth-child(2) > .popup-group-form-input > .dx-show-invalid-badge > .dx-texteditor-container > .dx-texteditor-input-container > .dx-texteditor-input').type('Pass23'); // password
+        cy.get('.dx-dropdowneditor-input-wrapper > .dx-texteditor-container > .dx-texteditor-input-container > .dx-texteditor-input').should('be.visible').click({force:true});
+        cy.wait(200);
+        cy.get(':nth-child(1) > .dx-show-invalid-badge > .dx-texteditor-container > .dx-texteditor-input-container > .dx-texteditor-input').click({force:true}).type("ADMIN").type('{enter}');
+        cy.wait(200);
+        cy.get('[aria-describedby="dx-col-30"] > :nth-child(1) > .dx-datagrid-search-text').should('be.visible').click({force:true});
+        cy.wait(200);
+        cy.get('#submit-link > .dx-buttongroup > .dx-buttongroup-wrapper > .dx-button-has-text > .dx-button-content').should('be.visible').click({force:true});
+        })
+});
+
+
+
+
+
+
---
+++ cypress/support/commands.js
@@ -0,0 +1,17 @@
+require('cypress-xpath');
+
+// Define the custom login command
+Cypress.Commands.add('login', (company_name,email, password) => {
+    cy.xpath('//div[@class="dx-texteditor-input-container"]//input[@type="email"]').type("lee2@greenstem.com.my");
+    cy.xpath('//div[@class="dx-texteditor-input-container"]//input[@type="text"]').type("greenplus");
+    cy.xpath('//div[@class="dx-texteditor-input-container"]//input[@type="password"]').type("123");
+    cy.get('.dx-button-content').click();
+});
+Cypress.Commands.add('selectRandomDropdownOption', (dropdownSelector) => {
+    cy.get(dropdownSelector).click(); // Open the dropdown
+    cy.get('.dx-list-item').then($options => { // Adjust selector if needed
+      const randomIndex = Math.floor(Math.random() * $options.length);
+      cy.wrap($options[randomIndex]).click();
+    });
+  });
+  
